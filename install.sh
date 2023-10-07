23e9f249847e6b3aa589e73085de72617d486636
Jakub Jendryka <jendryka.j@gmail.com>
2023-10-07 14:34:34 +0200

    Change installation to use pyproject.toml

--- install.sh
+++
@@ -1,9 +0,0 @@
-python3 -m venv .venv > /dev/null 2>&1
-.venv/bin/python3 -m pip install .
-
-echo '#!/usr/bin/env bash' > qms-backend.sh
-echo "$(pwd)/.venv/bin/python3 $(pwd)/src/main.py \"\$@\"">> qms-backend.sh
-chmod +x qms-backend.sh
-
-mkdir --parents "$HOME/.local/bin/"
-ln -sf "$(pwd)/qms-backend.sh" "$HOME/.local/bin/qms-backend"
--- src/main.py
+++ src/main.py
@@ -1,2 +1,6 @@
-if __name__ == "__main__":
+def main() -> None:
     print("Hello in QMS-backend")
+
+
+if __name__ == "__main__":
+    main()
